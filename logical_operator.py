4539dad7a3567fec64591ac53a315796d7d1dd89
dimeneman <146258763+dimeneman@users.noreply.github.com>
2024-05-09 16:18:51 +0530

    Description
    Logical operator helps us to write down mathematical and reasoning code . This file will help to understand the basic use of logical operator in python language.

---
+++ logical_operator.py
@@ -0,0 +1,20 @@
+# and operator --> both of the conditions should be true
+
+my_favourite_number = int(input())
+print("my enterd number is" ,my_favourite_number)
+
+if my_favourite_number > 0 and my_favourite_number <= 3:
+ print("true")
+else:
+ print("false")
+
+
+# or operator --> only one conditionn  should be true 
+
+my_favourite_number = int (input())
+print ( "enter your number again")
+
+if my_favourite_number > 0 or my_favourite_number <= 3 :
+ print("true")
+else:
+ print("false")

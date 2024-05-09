9a42508bc16feafd87d805c3133dbcad227d4070
dimeneman <146258763+dimeneman@users.noreply.github.com>
2024-05-09 18:00:22 +0530

    Description
    Multiple assignment is the ability to assign multiple variables to unpacked values within one statement. This allows for code to be more concise and readable, and is done by separating the variables to be assigned with a comma such as first, second, third = (1,2,3) or for index, item in enumerate(iterable) .
    Don's forget to try this out.

---
+++ multiple_assighnment.py
@@ -0,0 +1,15 @@
+#MULTIPLE ASSIGHMENT = allows us to assign multiple variables at the same time in one line of code...
+
+name = "EMAN"
+age = 18 
+height = 250.5
+handsome = True 
+rich = False
+
+print (name , age , height, handsome , rich )
+
+# we can write this code in one line ... this is multiple assighnment
+
+
+Name , Age , Height , Handsome, Rich = "EMAN", 18, 250.5,True,False
+print (Name , Age , Height, Handsome , Rich )

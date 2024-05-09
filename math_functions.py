769fc4c7007b06adee5d14399bfbd17156171084
dimeneman <146258763+dimeneman@users.noreply.github.com>
2024-05-09 16:32:58 +0530

    Description
    This code will helps us to understand the mathematical logic by logical operator.

---
+++ math_functions.py
@@ -0,0 +1,23 @@
+import math  #  THIS IS FOR USING BASIC MATH FUNCTIONS  
+
+pi = 3.14
+print (round(pi))   # TO GET THE NEAREST INTEGER 
+
+print (math.ceil(pi))   # TO GET THE UPPER INTEGER OF THE TAKEN VALUE
+
+print (math.floor(pi))   # TO GET THE LOWEST INTEGER OF THE TAKEN VALUE
+
+print ( abs (pi))     # abs = absolute value , that's why it will just show the
+                    # value of how far is the value form 0 ...
+
+print ( math.sqrt(pi))   # TO GET THE SQUARE ROOT OF AN PARTICULAR FUNCTION
+
+print (pow(pi,2))   # TO MULTIPLY ACCORDING TO THE POWER !!!
+
+x = 5
+y = 10
+z = 4
+
+print (max(x,y,z))   #  TO GET THE MAXIMUM VALUE BY COMPARING 
+
+print (min(x,y,z))   # TO GETT THE MINIMUM VALUE BY COMPARING  

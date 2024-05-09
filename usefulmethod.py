a6494fc5268c9c3b04c48df40d3e773d218cd76e
dimeneman <146258763+dimeneman@users.noreply.github.com>
2024-05-09 22:07:10 +0530

    Description
    This file contains some useful things in python. You can check it out .

---
+++ usefulmethod.py
@@ -0,0 +1,35 @@
+name = "Eman Gope"
+
+# checking length 
+print(len(name))
+
+# finding a charater
+print( name.find("G"))
+
+# capitalize the first  character
+print ( name.capitalize())
+
+# making the letters uppercase
+print (name.upper())
+
+# making the letters lowercase
+print (name.lower())
+
+# asking the string is digit or not 
+print (name.isdigit())
+
+number = "123"
+print (number.isdigit())
+
+# checking the string is alphabet or not 
+print (name.isalpha())  # alpha is the short form
+print (number.isalpha())
+
+# counting any character in a string 
+print ( name.count("e"))
+
+# replacing a character with another character
+print (name.replace("G","S"))
+
+# printing the string more than ones 
+print (name*3)

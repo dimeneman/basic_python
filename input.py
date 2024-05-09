cc507ad35adf04ad58057e49405d7e7c142e274e
dimeneman <146258763+dimeneman@users.noreply.github.com>
2024-05-09 17:43:53 +0530

    Description
    From this code we will learn how to  take input in python language . In  this code we will take input and we will perform some specific operation .

---
+++ input.py
@@ -0,0 +1,19 @@
+ # HOW TO TAKE A INPUT ... GENERALLY FOR TAKING INPUT 
+# WE USE "input()"" this code but for assigning some value we have
+# to assign it with a variable 
+# GENERALLY THE "input()" CODE IS FOR STRING VALUES BUT IF WE 
+# WANT TO TAKE THEH INPUT AS INTEGER AND FLOAT WE HAVE TO WRITE DOWN THE DATATYPE NAME like----
+# "int(input())"  &  "float(input())"
+
+name = input("WHAT IS YOUR NAME ==>")
+age = int(input ("HOW OLD ARE YOU ==>"))
+height = float(input("HOW TALL ARE YOU ==>"))
+ 
+                           # WE HAVE TO USE TYPECASTING 
+
+print ( "Hello " + str(name) )      
+print ( "you're " +str (age) + " years old" )
+print ( "you're height is " + str( height) + " cm")
+
+
+                    #  TAKING INPUT IS a very important job !!
